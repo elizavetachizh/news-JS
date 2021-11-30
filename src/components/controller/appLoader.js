dfed570aeee9ed047f29e43482e1c8b1f8289399
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2021-11-30 11:47:09 +0300

    refactor: add new files, add webpack

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b693fea9f5fe4225bcb60d362b247ace', // получите свой ключ https://newsapi.org/
         });
     }
 }
